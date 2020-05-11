32300c3b68081a637c00107a9f384ed6d7f16e1f
Alex <pasechnyy@proitr.ru>
2020-05-11 10:13:02 +0300

    карточки

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-service app-service_dark">
+      <Card v-for="i in 5" :key="i"></Card>
+    </div>
+    <div class="app-enter"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Card
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +23,26 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns:minmax(230px, 1300px);
+  grid-template-rows: max-content 1fr;
+  background-color: wheat;
+  justify-content: center;
+}
+.app-service {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+  grid-gap: 25px;
+  padding: 25px;
+  border-radius: 10px;
+}
+.app-service_dark {
+  background-color: #07273c;
+}
+@media (max-width: 1290px) {
+  #app {
+  grid-template-columns:minmax(230px, 720px);
+}
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card card_white">
+    <img src="https://mdoc.nces.by/img/input.png" />
+    <h2>вход</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  background-color: #41535f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  min-height: 150px;
+  max-height: 230px;
+}
+.card_white {
+  color: white;
+}
+.card img {
+  padding: 12px;
+  border: 1px solid white;
+  border-radius: 10%;
+}
+</style>
